a259cf05a96b25126b597dc047889bf271513332
ckost9n <ckost9n@gmail.com>
2022-05-02 16:00:20 +0300

    First UI setup

---
+++ Alias Party/Controllers/RulesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RulesViewController.swift
+//  Alias Party
+//
+//  Created by Konstantin on 02.05.2022.
+//
+
+import UIKit
+
+class RulesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Alias Party/Supports/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Alias Party
+//
+//  Created by Konstantin on 02.05.2022.
+//
+
+import Foundation
