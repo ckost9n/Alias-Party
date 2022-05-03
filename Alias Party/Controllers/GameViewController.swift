6d062c9b7458a92e877b5a4e3ca16fb8dfc598f9
ckost9n <ckost9n@gmail.com>
2022-05-03 21:12:17 +0300

    update worldLabel numberOfLines

--- Alias Party/Controllers/GameViewController.swift
+++ Alias Party/Controllers/GameViewController.swift
@@ -20,8 +20,14 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupViews()
+        
         timerLabel.text = String(secondsRemaining)
         createTimer()
+    }
+    
+    private func setupViews() {
+        wordLabel.numberOfLines = 0
     }
     
     @IBAction func rightButtonPressed(_ sender: UIButton) {
